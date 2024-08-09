dffc211170177291577f678cf7c1e1a9d090ac85
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2024-08-09 15:03:00 +0400

    Initial Commit Learning LangChain
    Text -> Model -> Output

---
+++ main.py
@@ -0,0 +1,20 @@
+from langchain.llms import LlamaCpp
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+llm = LlamaCpp(
+    model_path="D:\Mine\RAG_LLMS\llm_rag_tut\mistral-7b-v0.1.Q8_0.gguf",
+    temprature=0.75,
+    max_tokens=2000,
+    top_p=1,
+    callback_manager=callback_manager,
+    verbose=True,
+)
+
+if __name__ == '__main__':
+    while True:
+        print("")
+        question = input("What is your query: ")
+        llm(question)
