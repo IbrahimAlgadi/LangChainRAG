99e0b636cbbba70de5978f73d065ddd48170b359
Ibrahim Algadi <ibrahimalgadi.email@gmail.com>
2024-08-09 15:19:16 +0400

    Using stream instead of callback manager

--- main_2.py
+++ main_2.py
@@ -1,11 +1,15 @@
 from langchain.llms import LlamaCpp
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.prompts import PromptTemplate
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
 llm = LlamaCpp(
     model_path="D:\Mine\RAG_LLMS\llm_rag_tut\mistral-7b-v0.1.Q8_0.gguf",
     temperature=0.75,
     max_tokens=2000,
     top_p=1,
+    callback_manager=callback_manager,
     verbose=True,
 )
 
@@ -17,5 +21,4 @@ if __name__ == '__main__':
         template="How to cook {recipe}"
     )
     chain = question | llm
-    for chunk in chain.stream({"recipe": "Boiled Egg"}):
-        print(chunk, end="", flush=True)
+    chain.invoke({"recipe": "Boiled Egg"})
